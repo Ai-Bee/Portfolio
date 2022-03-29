1bb331841c17e7db6f293bb1bed0e9137412d903
Iboro Inyang <Iboro.Inyang@sterling.ng>
2022-03-30 00:30:10 +0100

    Redesigned the homepage

--- src/App.js
+++ src/App.js
@@ -4,10 +4,38 @@ import TheNav from './parts/navPart';
 import About from './parts/about';
 import Contact from './parts/contact';
 import Projects  from './parts/projects';
+import { useEffect } from 'react';
 
 function App() {
+  useEffect(() => {
+    const cursor = document.querySelector(".cursor");
+    let timeout;
+
+    //follow cursor on mousemove
+    document.addEventListener("mousemove", (e) => {
+      let x = e.pageX;
+      let y = e.pageY;
+
+      cursor.style.top = y + "px";
+      cursor.style.left = x + "px";
+      cursor.style.display = "block";
+
+      //cursor effects when mouse stopped
+      function mouseStopped(){
+        cursor.style.display = "none";
+      }
+      clearTimeout(timeout);
+      timeout = setTimeout(mouseStopped, 1000);
+    });
+
+    //cursor effects when mouseout
+    document.addEventListener("mouseout", () => {
+      cursor.style.display = "none";
+    });
+  }, [])
   return (
     <div className="App">
+     <div className="cursor"></div> 
         <Jumbotron/>
         <TheNav/>
         <About/>
--- src/parts/jumbotron.js
+++ src/parts/jumbotron.js
@@ -7,44 +7,60 @@ import Slide from 'react-reveal/Slide'
 function Jumbotron() {
   return (
     <div className="container-fluid" id="home">
-        <div className="banner-row row justify-content-center">
-            <div className="col-lg-5 theText">
-                <Slide right>
-                <h3>I'm <span>Iboro</span></h3>
-                <h1>Code Architect</h1>
-                </Slide>
-                <Slide right>
-                <p  className="mx-2">
-                    I'm a Software Engineer and I love
-                    to build amazing things using
-                    various technologies. I'm a
-                    creative thinker who is great at
-                    problem solving and team work.
-                </p>
-                </Slide>
-                <Fade top cascade>
-                <div className="socialLinks mt-4">
-                    <a href="#" target="_blank" className="px-4">
-                        <img src="./instagram.png" alt="instagram logo"/>
-                    </a>
-                    <a href="#" target="_blank" className="px-4">
-                        <img src="./twitter.png" alt="twitter logo"/>
-                    </a>
-                    <a href="#" target="_blank" className="px-4">
-                        <img src="./github-sign.png" alt="github logo"/>
-                    </a>
-                    <a href="#" target="_blank"  className="pl-4">
-                        <img src="./linkedin.png" alt="linkedin logo"/>
-                    </a>
-                </div>
-                </Fade>
-            </div>
-            <Zoom>
-            <div className="col-lg-6 me-image" style={{backgroundImage: 'url(./fifty.svg)'}}>
+        <div class="bars" id="nav-action">
+  <span class="bar"> </span>
+</div>
+      <div className="banner-row row justify-content-center" style={{ backgroundImage: 'url(./illustration.png)' }}>
+        <div className='col text-straight row'>
+          
+          <span className="straight">|||||||||||||||||||||||||||||||||||</span>
+          <a href="#"
+            target="_blank" rel="noreferrer" className="col mb-4">
+            <img src="./instagram.png" alt="instagram logo" />
+          </a>
+          <a href="#" rel="noreferrer"
+            target="_blank" className=" mb-4 col">
+            <img src="./twitter.png" alt="twitter logo" />
+          </a>
+          <a href="#" target="_blank"
+            rel="noreferrer" className=" mb-4 col">
+            <img src="./github-sign.png" alt="github logo" />
 
-            </div>
-            </Zoom>
+          </a>
+          <a href="#" target="_blank" rel="noreferrer" className=" mb-4 col">
+            <img src="./linkedin.png" alt="linkedin logo" />
+          </a>
         </div>
+        <div className="col-lg-5 theText">
+         
+          <Slide right>
+            <h3>I'm <span>Iboro</span></h3>
+            <h1>Code Architect</h1>
+          </Slide>
+          <Slide right>
+            <p className="mx-2">
+              I'm a Software Engineer and I love
+              to build amazing things using
+              various technologies. I'm a
+              creative thinker who is great at
+              problem solving and team work.
+            </p>
+          </Slide>
+          <Fade top cascade>
+          </Fade>
+        </div>
+        <Zoom>
+          <div className="col-lg-6 me-image" >
+
+            <div className='texts'>
+              <h1>WORK</h1>
+              <h1>ABOUT</h1>
+              <h1>CONTACT</h1>
+            </div>
+
+          </div>
+        </Zoom>
+      </div>
     </div>
   );
 }
